c37393e460d0fa0ed0567a4a2fbdef788f3eec58
klashok <erashokit@gmail.com>
2024-11-19 23:13:26 +0530

    Test Models

--- resources/views/layouts/inc/frontend-mobile-navigation.blade.php
+++ resources/views/layouts/inc/frontend-mobile-navigation.blade.php
@@ -24,9 +24,17 @@
                 <nav>
                     <ul>
                          <li class="current-menu-item"><a href="{{ url('/') }}">For You</a></li>
-                                @php
+                         @php
+                            use App\Models\Category;
+
+                            $categories = Category::where('navbar_status', '1')
+                                ->where('status', '1')
+                                ->get();
+                        @endphp
+
+                                {{-- @php
                                     $categories = App\Models\Category::where('navbar_status', '1')->where('status','1')->get();
-                                @endphp
+                                @endphp --}}
                                 @foreach ($categories as $catitem)
                                     <li><a href="{{ url($catitem->slug) }}">{{ $catitem->name }}</a></li>
                                 @endforeach
